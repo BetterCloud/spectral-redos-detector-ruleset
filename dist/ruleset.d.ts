012ffaffa3b063c33370eefbcf3f856fc96f6d45
Brandon Jones <brandon.jones@bettercloud.com>
2023-01-30 14:34:54 -0500

    Add back one rule

--- dist/ruleset.d.ts
+++ dist/ruleset.d.ts
@@ -1,5 +1,18 @@
+import * as _stoplight_spectral_core from '@stoplight/spectral-core';
+import { DiagnosticSeverity } from '@stoplight/types';
+
 declare const _default: {
-    rules: {};
+    rules: {
+        "unsafe-pattern-regex-components-schema-property": {
+            description: string;
+            given: string;
+            message: string;
+            severity: DiagnosticSeverity;
+            then: {
+                function: _stoplight_spectral_core.RulesetFunctionWithValidator<any, unknown>;
+            };
+        };
+    };
 };
 
 export { _default as default };
--- src/ruleset.ts
+++ src/ruleset.ts
@@ -1,18 +1,18 @@
-// import validateSchemaPropertyPatternRegex from "./functions/validateSchemaPropertyPatternRegex";
-// import { DiagnosticSeverity } from "@stoplight/types";
+import validateSchemaPropertyPatternRegex from "./functions/validateSchemaPropertyPatternRegex";
+import { DiagnosticSeverity } from "@stoplight/types";
 
 export default {
   rules: {
-    // "unsafe-pattern-regex-components-schema-property": {
-    //   description:
-    //     "Check for possible ReDos regex patterns in components schemas properties",
-    //   given: "$.components.schemas..properties[*]",
-    //   message: "{{error}}",
-    //   severity: DiagnosticSeverity.Error,
-    //   then: {
-    //     function: validateSchemaPropertyPatternRegex,
-    //   },
-    // },
+    "unsafe-pattern-regex-components-schema-property": {
+      description:
+        "Check for possible ReDos regex patterns in components schemas properties",
+      given: "$.components.schemas..properties[*]",
+      message: "{{error}}",
+      severity: DiagnosticSeverity.Error,
+      then: {
+        function: validateSchemaPropertyPatternRegex,
+      },
+    },
     // "unsafe-pattern-regex-components-schema-array-property": {
     //   description:
     //     "Check for possible ReDos regex patterns in components schemas with array items that are not objects",
