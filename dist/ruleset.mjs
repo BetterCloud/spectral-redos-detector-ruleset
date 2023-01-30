aa89c1a96c9340703a56c4947b6d3ec7299853bd
Brandon Jones <brandon.jones@bettercloud.com>
2023-01-30 14:47:40 -0500

    Uncomment rule

--- dist/ruleset.mjs
+++ dist/ruleset.mjs
@@ -3557,6 +3557,15 @@ var ruleset_default = {
       then: {
         function: validateSchemaPropertyPatternRegex_default
       }
+    },
+    "unsafe-pattern-regex-components-headers-property": {
+      description: "Check for possible ReDos regex patterns in components headers schemas",
+      given: "$.components.headers..schema",
+      message: "{{error}}",
+      severity: DiagnosticSeverity.Error,
+      then: {
+        function: validateSchemaPropertyPatternRegex_default
+      }
     }
   }
 };
--- src/ruleset.ts
+++ src/ruleset.ts
@@ -35,16 +35,16 @@ export default {
         function: validateSchemaPropertyPatternRegex,
       },
     },
-    // "unsafe-pattern-regex-components-headers-property": {
-    //   description:
-    //     "Check for possible ReDos regex patterns in components headers schemas",
-    //   given: "$.components.headers..schema",
-    //   message: "{{error}}",
-    //   severity: DiagnosticSeverity.Error,
-    //   then: {
-    //     function: validateSchemaPropertyPatternRegex,
-    //   },
-    // },
+    "unsafe-pattern-regex-components-headers-property": {
+      description:
+        "Check for possible ReDos regex patterns in components headers schemas",
+      given: "$.components.headers..schema",
+      message: "{{error}}",
+      severity: DiagnosticSeverity.Error,
+      then: {
+        function: validateSchemaPropertyPatternRegex,
+      },
+    },
     // "unsafe-pattern-regex-path-parameter-property": {
     //   description: "Check regex patterns in path parameters",
     //   given: "$.paths.*.*.parameters[*].schema",
