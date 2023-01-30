8fc9851dcc8bc3d41544eb01bcf62bde414fab8e
Brandon Jones <brandon.jones@bettercloud.com>
2023-01-30 14:21:09 -0500

    Comment out rules

--- src/ruleset.ts
+++ src/ruleset.ts
@@ -24,35 +24,35 @@ export default {
     //     function: validateSchemaPropertyPatternRegex,
     //   },
     // },
-    "unsafe-pattern-regex-components-schema-array-object-property": {
-      description:
-        "Check for possible ReDos regex patterns in components schemas with array items that are objects",
-      given:
-        '$.components.schemas..properties[?(@ && @.type=="array")].items.properties[*]',
-      message: "{{error}}",
-      severity: DiagnosticSeverity.Error,
-      then: {
-        function: validateSchemaPropertyPatternRegex,
-      },
-    },
-    "unsafe-pattern-regex-components-headers-property": {
-      description:
-        "Check for possible ReDos regex patterns in components headers schemas",
-      given: "$.components.headers..schema",
-      message: "{{error}}",
-      severity: DiagnosticSeverity.Error,
-      then: {
-        function: validateSchemaPropertyPatternRegex,
-      },
-    },
-    "unsafe-pattern-regex-path-parameter-property": {
-      description: "Check regex patterns in path parameters",
-      given: "$.paths.*.*.parameters[*].schema",
-      message: "{{error}}",
-      severity: DiagnosticSeverity.Error,
-      then: {
-        function: validateSchemaPropertyPatternRegex,
-      },
-    },
+    // "unsafe-pattern-regex-components-schema-array-object-property": {
+    //   description:
+    //     "Check for possible ReDos regex patterns in components schemas with array items that are objects",
+    //   given:
+    //     '$.components.schemas..properties[?(@ && @.type=="array")].items.properties[*]',
+    //   message: "{{error}}",
+    //   severity: DiagnosticSeverity.Error,
+    //   then: {
+    //     function: validateSchemaPropertyPatternRegex,
+    //   },
+    // },
+    // "unsafe-pattern-regex-components-headers-property": {
+    //   description:
+    //     "Check for possible ReDos regex patterns in components headers schemas",
+    //   given: "$.components.headers..schema",
+    //   message: "{{error}}",
+    //   severity: DiagnosticSeverity.Error,
+    //   then: {
+    //     function: validateSchemaPropertyPatternRegex,
+    //   },
+    // },
+    // "unsafe-pattern-regex-path-parameter-property": {
+    //   description: "Check regex patterns in path parameters",
+    //   given: "$.paths.*.*.parameters[*].schema",
+    //   message: "{{error}}",
+    //   severity: DiagnosticSeverity.Error,
+    //   then: {
+    //     function: validateSchemaPropertyPatternRegex,
+    //   },
+    // },
   },
 };
